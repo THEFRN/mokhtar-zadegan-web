fb005978b607f750bb74e8ea2eece15b648120fc
Ahmad <farhangian.ahmad@gmail.com>
2023-07-25 17:15:33 +0330

    Migrated!

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from . models import CustomUser
 
-# Register your models here.
+
+@admin.register(CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    model = CustomUser
+    list_display = ['username', 'email', 'first_name', 'last_name', 'address']
+
--- accounts/models.py
+++ accounts/models.py
@@ -2,12 +2,12 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.urls import reverse
 
-class CustomUser(AbstractUser):
-    first_name = models.CharField(max_length=60)
-    last_name = models.CharField(max_length=60)
-    phone_number = models.PositiveIntegerField(max_length=11)
-    address = models.TextField()
 
+class CustomUser(AbstractUser):
+    first_name = models.CharField(max_length=60, blank=True)
+    last_name = models.CharField(max_length=60, blank=True)
+    phone_number = models.PositiveIntegerField(null=True)
+    address = models.TextField(blank=True)
 
     def __str__(self) -> str:
         return self.first_name
